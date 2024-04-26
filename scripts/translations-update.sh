3cff68e918872701109b230ce358886841da5a36
Henrik Tholle <ht@bellcom.dk>
2024-04-26 14:23:21 +0200

    Update to drupal 10

--- scripts/translations-update.sh
+++ scripts/translations-update.sh
@@ -3,8 +3,8 @@ ROOT_DIR=$(realpath "$(dirname $0)/../")
 DRUSH_EXEC=$(which drush)
 
 echo 'Exporting existing translations.'
-mkdir -p $ROOT_DIR/translations/translations-dumps/
-$DRUSH_EXEC language-export --langcodes=da --file=$ROOT_DIR/translations/translations-dumps/da-dump-$(date +'%Y%m%d-%H%M%S').po
+#mkdir -p $ROOT_DIR/translations/translations-dumps/
+#$DRUSH_EXEC language-export --langcodes=da --file=$ROOT_DIR/translations/translations-dumps/da-dump-$(date +'%Y%m%d-%H%M%S').po
 
-echo "Importing translations from $ROOT_DIR/translations/os2core-drupal-8.x.da.po."
-$DRUSH_EXEC language-import --langcode=da $ROOT_DIR/translations/os2core-drupal-8.x.da.po
+#echo "Importing translations from $ROOT_DIR/translations/os2core-drupal-8.x.da.po."
+#$DRUSH_EXEC language-import --langcode=da $ROOT_DIR/translations/os2core-drupal-8.x.da.po
